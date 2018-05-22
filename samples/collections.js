5e888809b9b25b67a457f7a442c99d84ed389055
northlight <northlight2015@gmail.com>
2018-05-22 18:12:46 +0300

    src/bynder-js-sdk.js

--- samples/collections.js
+++ samples/collections.js
@@ -28,3 +28,25 @@ bynder.getCollections({
     .catch((error) => {
         console.log(error);
     });
+
+bynder.addMediaToCollection({
+    id: '000000-0000-0000-0000000000000000',
+    data: [
+        '000000-0000-0000-0000000000000000',
+        '000000-0000-0000-0000000000000000'
+    ]
+})
+      .then((data) => {
+          console.log('addMediaToCollection', data, '\n\n');
+          return bynder.shareCollection({
+              id: '000000-0000-0000-0000000000000000',
+              recipients: 'user1@bynder.com,user2@bynder.com',
+              collectionOptions: 'edit'
+          });
+      })
+      .then((data) => {
+          console.log('shareCollection', data, '\n\n');
+      })
+      .catch((error) => {
+          console.log(error);
+      });
