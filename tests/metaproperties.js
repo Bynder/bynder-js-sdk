9354015405c907cf8c2713c73d2e1633501b282a
northlight <northlight2015@gmail.com>
2018-05-24 22:03:40 +0300

    add tests

--- tests/metaproperties.js
+++ tests/metaproperties.js
@@ -7,6 +7,7 @@ const byndertestproperty = 'byndertestproperty';
 const label = 'label';
 const type = 'select';
 let metaId;
+let optionId;
 
 describe('Save metaproperty', () => {
     let bynder;
@@ -93,37 +94,6 @@ describe('Get metaproperty', () => {
     });
 });
 
-describe('Get options of metaproperty', () => {
-    let bynder;
-    let options;
-
-    beforeEach((done) => {
-        bynder = new Bynder(configs);
-
-        bynder.getOptionsOfMetaproperty({
-            ids: metaId
-        }).then((data) => {
-            options = data;
-            done();
-        })
-        .catch((error) => {
-            options = error;
-            done();
-        });
-    });
-
-    it('Get options from metaproperty', () => {
-        expect(options.constructor).toEqual(Object);
-        const metapropertyKeys = Object.keys(options);
-        console.log(metapropertyKeys);
-        // expect(metapropertyKeys).toContain('name');
-        // expect(metapropertyKeys).toContain('id');
-        // expect(metapropertyKeys).toContain('options');
-        // expect(metapropertyKeys).toContain('zindex');
-        // expect(metapropertyKeys).toContain('isFilterable');
-    });
-});
-
 describe('Edit metaproperty', () => {
     let bynder;
     let response;
@@ -159,6 +129,111 @@ describe('Edit metaproperty', () => {
         expect(metapropertyKeys).toContain('options');
         expect(metapropertyKeys).toContain('zindex');
         expect(metapropertyKeys).toContain('isFilterable');
+    });
+});
+
+describe('Create an option to metaproperty', () => {
+    let bynder;
+    let result;
+
+    beforeEach((done) => {
+        bynder = new Bynder(configs);
+
+        bynder.createMetapropertyOption({
+            id: metaId,
+            name: 'optiontest'
+        }).then((data) => {
+            result = data;
+            done();
+        })
+        .catch((error) => {
+            result = error;
+            done();
+        });
+    });
+
+    it('Get options from metaproperty', () => {
+        expect(result.constructor).toEqual(Object);
+        const metapropertyKeys = Object.keys(result);
+        expect(metapropertyKeys).toContain('statuscode');
+        expect(metapropertyKeys).toContain('message');
+        expect(result.statuscode).toBe(201);
+        expect(result.message).toBe('Created');
+    });
+});
+
+describe('Modify an option of metaproperty', () => {
+    let bynder;
+    let result;
+
+    beforeEach((done) => {
+        bynder = new Bynder(configs);
+
+        bynder.getMetaproperty({
+            id: metaId
+        })
+        .then((data) => {
+            optionId = data.options[0].id;
+        })
+        .then(() => {
+            return bynder.editMetapropertyOption({
+                id: metaId,
+                optionId,
+                lable: 'Option Test'
+            });
+        }).then((data) => {
+            result = data;
+            done();
+        })
+        .catch((error) => {
+            result = error;
+            done();
+        });
+    });
+
+    it('Modify an option of metaproperty', () => {
+        expect(result.constructor).toEqual(Object);
+        const metapropertyKeys = Object.keys(result);
+        expect(metapropertyKeys).toContain('statuscode');
+        expect(metapropertyKeys).toContain('message');
+        expect(result.statuscode).toBe(201);
+        expect(result.message).toBe('Created');
+    });
+});
+
+describe('Delete an option of metaproperty', () => {
+    let bynder;
+    let result;
+
+    beforeEach((done) => {
+        bynder = new Bynder(configs);
+
+        bynder.getMetaproperty({
+            id: metaId
+        })
+        .then((data) => {
+            optionId = data.options[0].id;
+        })
+        .then(() => {
+            return bynder.deleteMetapropertyOption({
+                id: metaId,
+                optionId
+            });
+        }).then((data) => {
+            result = data;
+            done();
+        })
+        .catch((error) => {
+            result = error;
+            done();
+        });
+    });
+
+    it('Modify an option of metaproperty', () => {
+        expect(result).not.toBeUndefined();
+        expect(result.constructor).toEqual(Object);
+        const metapropertyKeys = Object.keys(result);
+        expect(metapropertyKeys.length).toBe(0);
     });
 });
 
