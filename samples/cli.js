43690dec4e99b0020cbc6d308c475c7dbf5d6574
Bugra Okcu <bugra.okcu@bynder.com>
2021-01-13 14:01:28 +0100

    update the sample file

--- samples/cli.js
+++ samples/cli.js
@@ -1,39 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const yargs = require('yargs'); // eslint-disable-line import/no-extraneous-dependencies
 
 const Bynder = require('../dist/bynder-js-sdk.cjs');
+const configs = require('../secret.example.json');
 
+const bynder = new Bynder(configs);
 
-let argv = yargs
-  .alias('b', 'base-url')
-  .alias('p', 'permanent-token')
-  .alias('f', 'file')
-  .usage('Usage: $0 [options]')
-  .example('$0 -b https://portal.getbynder.com/ -p permanenttoken')
-  .example('$0 -b https://portal.getbynder.com/ -p permanenttoken -f /path/to/my/file.jpg')
-  .describe('b', 'Base url of the portal')
-  .describe('p', 'Permanent token')
-  .describe('f', 'Path of the file to upload - optional')
-  .help('h')
-  .alias('h', 'help')
-  .argv;
-
-const bynder = new Bynder({
-  baseURL: argv['base-url'],
-  permanentToken: argv['permanent-token']
-});
+let brand;
 
 bynder.getBrands()
   .then((data) => {
-    const brand = data[0];
-    const file = argv['file'] || `${__dirname}/testasset.png`;
+    console.log(data)
+    brand = data[0];
+    const file = `${__dirname}/testasset.png`;
     return bynder.uploadFile({
       filename: path.basename(file),
       body: fs.readFileSync(file),
       data: {
         brandId: brand.id,
         name: 'test asset'
+      }
+    });
+  })
+  .then((data) => {
+    console.log(data)
+    const file = `${__dirname}/testasset.png`;
+    return bynder.uploadFile({
+      filename: path.basename(file),
+      body: fs.readFileSync(file),
+      data: {
+        brandId: brand.id,
+        name: 'test asset new version',
+        mediaId: data['mediaid']
       }
     });
   })
@@ -49,3 +47,5 @@ function printResponse(data) {
   console.log(data); // eslint-disable-line no-console
   process.exit(0);
 }
+
+
