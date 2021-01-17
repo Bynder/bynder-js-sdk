1753746394ec8d005ba684d681548b335fc5684f
Bugra Okcu <bugra.okcu@bynder.com>
2021-01-18 00:29:49 +0100

    fix call and remove package

--- samples/cli.js
+++ samples/cli.js
@@ -6,12 +6,10 @@ const configs = require('../secret.example.json');
 
 const bynder = new Bynder(configs);
 
-let brand;
-
 bynder.getBrands()
   .then((data) => {
     console.log(data)
-    brand = data[0];
+    const brand = data[0];
     const file = `${__dirname}/testasset.png`;
     return bynder.uploadFile({
       filename: path.basename(file),
@@ -29,8 +27,6 @@ bynder.getBrands()
       filename: path.basename(file),
       body: fs.readFileSync(file),
       data: {
-        brandId: brand.id,
-        name: 'test asset new version',
         mediaId: data['mediaid']
       }
     });
